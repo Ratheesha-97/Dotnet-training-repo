f8f98b0707c9d8d66bfc18bc5dc9689a5282dcdb
Ratheesha-97 <66117703+Ratheesha-97@users.noreply.github.com>
2021-09-10 09:41:39 +0530

    Add files via upload

---
+++ dot_netbasics/IfClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class IfClass
+    {
+        static void Main(string[] args)
+        {
+            int i = 10, j = 20;
+
+            if (i < j)
+            {
+                Console.WriteLine("i is less than j");
+            }
+
+            if (i > j)
+            {
+                Console.WriteLine("i is greater than j");
+            }
+        }
+    }
+}
